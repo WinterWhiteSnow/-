518767f62a918f5deaaba07c6c438725ba212a7a
Winter <kso8760@daum.net>
2021-09-02 23:10:46 +0900

    불량판매자까지 필터링하는 기능 추가

--- aladinscrap.py
+++ aladinscrap.py
@@ -1,5 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
+from buying_list import title_list
+from seller import seller_check
+
+
+def checking_buy(before_list):
+    result_result_list=[]
+    buying_title = title_list()
+    for i in before_list:
+        if i["title"] in buying_title:
+            pass
+        else:
+            result_result_list.append(i)
+    return  result_result_list       
 
 def extract_last_page():
     url = "https://www.aladin.co.kr/shop/wbrowse.aspx?ItemType=100&ViewRowsCount=24&ViewType=Simple&PublishMonth=0&SortOrder=6&page=2&UsedShop=0&PublishDay=84&CID=50932&CustReviewRankStart=&CustReviewRankEnd=&CustReviewCountStart=&CustReviewCountEnd=&PriceFilterMin=&PriceFilterMax=5000&SearchOption=&IsDirectDelivery=&QualityType=0&OrgStockStatus=&ShopNoCols=&IsUsedStore=0"
@@ -16,10 +29,11 @@ def extract_last_page():
             extract_page=extract_page[extract_page.find("'")+1:].replace("')","")
             extract_page = int(extract_page)
             last_page.append(extract_page)
-    last_page = int(last_page[0])
+    last_page = last_page[0]
     return last_page
 
-def scraping(last_page):
+def scraping():
+    last_page=extract_last_page()
     dict_result = []
     for i in range(1,last_page+1):
         print(f"알라딘 무협소설 scraping....{i}page")
@@ -33,19 +47,24 @@ def scraping(last_page):
             title = i.find_all("a",{"class":"bo"})
             price = i.find_all("span",{"class":"p1_bold"})
             for a,b in zip(title,price):
+                link = a["href"]
                 title_string = a.string
                 price_string = b.string
                 if "완결" in title_string:
                     title_string = title_string
                     price_string = price_string
-                    if "신무협" in title_string: #1.신무협 장편인경우, 2.신무협판타지일경우
-                        print(title_string+"은(는) 제목에 신무협이 있습니다.")
-                        dict_list = {
-                                "title":title_string,
-                                "price":price_string
-                            }
-                        dict_result.append(dict_list)
-                    else:    
+                    link = link
+                    if "신무협" in title_string: #1."신무협"인경우, 2.신무협판타지일경우
+                        if "판타지" in title_string or "환타지" in title_string:
+                            pass
+                        else:
+                            dict_list = {
+                                    "title":title_string[:title_string.find("1")],
+                                    "price":price_string,
+                                    "link":link
+                                }
+                            dict_result.append(dict_list)
+                    else:#제목에 신무협이 없는 경우    
                         input_title = title_string[:title_string.find("1")]
                         filter_url = f"https://book.naver.com/search/search.nhn?sm=sta_hty.book&sug=&where=nexearch&query={input_title}"
                         r = requests.get(filter_url,headers=headers)
@@ -53,27 +72,37 @@ def scraping(last_page):
                         try:
                             ul = soup.find("ul",{"class":"basic"}).find("li").find("dl").find("span").string
                             if "신무협" in ul: #괄호에 신무협이 있는경우
-                                title_string = title_string
-                                price_string = price_string
-                                print(title_string+"은(는) 괄호에 신무협이라고 적혀있다.")
-                                dict_list = {
-                                    "title":title_string,
-                                    "price":price_string
-                                }
-                                dict_result.append(dict_list)
-                            else:# 없는 경우 1. 정말로 신무협이 아닐경우, 2. 신무협은 맞는데 소개글에 적힌경우
-                                introduce_text = soup.find("ul",{"class":"basic"}).find("li").find("dl").find("dd",{"id":"searchDescrition_10495767"}).text
-                                if "신무협" in introduce_text:
+                                if "판타지" in ul and "환타지" in ul:
+                                    pass
+                                else:
                                     title_string = title_string
                                     price_string = price_string
-                                    print(title_string+"은(는) 소개글에 신무협이라고 적혀있다.")
                                     dict_list = {
-                                    "title":title_string,
-                                    "price":price_string
+                                        "title":title_string[:title_string.find("1")],
+                                        "price":price_string,
+                                        "link":link
                                     }
                                     dict_result.append(dict_list)
+                            else:# 없는 경우 1. 정말로 신무협이 아닐경우, 2. 신무협은 맞는데 소개글에 적힌경우
+                                introduce_text = soup.find("ul",{"class":"basic"}).find("li").find("dl").find("dd",{"type":"책소개"}).text
+                                if "신무협" in introduce_text:
+                                    if "판타지" in introduce_text or "환타지" in introduce_text:
+                                        pass
+                                    else:
+                                        title_string = title_string
+                                        price_string = price_string
+                                        dict_list = {
+                                        "title":title_string[:title_string.find("1")],
+                                        "price":price_string,
+                                        "link":link
+                                        }
+                                        dict_result.append(dict_list)
                                 else:   
-                                    print(title_string+"은(는) 신무협이 아닙니다.")
+                                    pass
                         except:
-                            pass    
-    return dict_result                    
+                            pass        
+    wow = checking_buy(dict_result)
+    seller_list = seller_check(wow)
+
+    return seller_list
+                       
---
+++ seller.py
@@ -0,0 +1,27 @@
+import requests
+from bs4 import BeautifulSoup
+
+def seller_check(urls):
+	seller_list = []
+	urls = urls
+	for i in urls:
+		url = i["link"]
+		headers = {'User-Agent':"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 Edg/92.0.902.73"}
+		r = requests.get(url,headers=headers)
+		soup = BeautifulSoup(r.text,"html.parser")
+		parent_tag = soup.find("div",{"class":"Ere_prod_bookwrap"}).find("div",{"class":"Ere_prod_Binfowrap"})
+		child_tag = parent_tag.find("div",{"class":"info"}).find("div",{"style":"margin-top:-25px;"})
+		seller = child_tag.find("div",{"class":"Ritem"}).find("a").string
+		if seller == "tkdjqdlsdlek":
+			pass
+		elif seller == "도쿠가와":
+			pass
+		elif seller == "만화뱅크":
+			pass
+		elif seller == "북코리아":
+			pass
+		else:
+			seller_list.append(i)
+	print(seller_list)		
+		
+	return seller_list
